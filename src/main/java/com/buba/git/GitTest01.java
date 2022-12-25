c66b54fa51ea53c42257f0ed8f109036387b1281
Jia <2513354198@qq.com>
2022-12-25 15:07:55 +0800

    first commit01

---
+++ src/main/java/com/buba/git/GitTest01.java
@@ -0,0 +1,12 @@
+package com.buba.git;
+
+/**
+ * @Author: 孔德嘉
+ * @Description: TODO
+ * @DateTime: 2022/12/22 17:46
+ **/
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
